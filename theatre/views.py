69d909486250849d7be0d7c840857537e33034f2
YOUR_NAME <YOUR_EMAIL>
2023-08-10 09:37:44 +0300

    added view to genre and actor

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from theatre.models import Genre, Actor
+from theatre.serializers import GenreSerializer, ActorSerializer
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
