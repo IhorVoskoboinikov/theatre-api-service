617a551f29dd0f5d61df8d034da046f18a2c19c1
YOUR_NAME <YOUR_EMAIL>
2023-08-10 09:37:17 +0300

    added serializers to genre and actor

---
+++ theatre/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from theatre.models import (
+    Genre,
+    Actor,
+    TheatreHall,
+    Play,
+    Performance,
+    Ticket,
+    Reservation,
+)
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
