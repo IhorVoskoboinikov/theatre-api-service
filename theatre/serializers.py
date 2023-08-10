922716193f4a146574ec547a8853e1e0cce77958
YOUR_NAME <YOUR_EMAIL>
2023-08-10 14:42:00 +0300

    added PlayListSerializer, PlayDetailSerializer, PlaySerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -28,3 +28,52 @@ class TheatreHallSerializer(serializers.ModelSerializer):
         model = TheatreHall
         fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
+
+class PlaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors",
+        )
+
+
+class PlayListSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+    actors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="full_name",
+    )
+
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors",
+        )
+
+
+class PlayDetailSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors",
+        )
