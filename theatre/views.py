0d3cdbe296614ee709cf186efec19ffc8cdebd7a
YOUR_NAME <YOUR_EMAIL>
2023-08-10 14:46:26 +0300

    added PlayViewSet with filters by title, actors, genres

--- theatre/views.py
+++ theatre/views.py
@@ -1,8 +1,16 @@
-from rest_framework import mixins
+from rest_framework import mixins, viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 
-from theatre.models import Genre, Actor, TheatreHall
-from theatre.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer
+from theatre.models import Genre, Actor, TheatreHall, Play
+from theatre.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    TheatreHallSerializer,
+    PlaySerializer,
+    PlayDetailSerializer,
+    PlayListSerializer
+)
 
 
 class GenreViewSet(
@@ -36,3 +44,51 @@ class TheatreHallViewSet(
     serializer_class = TheatreHallSerializer
     # authentication_classes = (JWTAuthentication,)
     # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class PlayViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the plays with filters"""
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._params_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self._params_to_ints(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+
+        return PlaySerializer
