a792175f3db2c1b968ecb0913ba5395266a6771f
YOUR_NAME <YOUR_EMAIL>
2023-08-10 09:30:49 +0300

    added better view in admin panel

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    TheatreHall,
+    Genre,
+    Actor,
+    Play,
+    Performance,
+    Reservation,
+    Ticket,
+)
+
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    fields = ("name",)
+    search_help_text = "Search by play"
+    list_per_page = 20
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    fields = ("name", "rows", "seats_in_row",)
+    search_fields = ("name",)
+    search_help_text = "Search by name"
+    list_per_page = 20
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    fields = ("first_name", "last_name",)
+    search_fields = ("first_name", "last_name")
+    search_help_text = "Search by first name or last name"
+    list_per_page = 20
+
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    fields = ("title", "genres", "actors")
+    search_fields = ("title",)
+    search_help_text = "Search by title"
+    list_per_page = 20
+
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    fields = ("play", "theatre_hall", "show_time",)
+    search_fields = ("play",)
+    search_help_text = "Search by play"
+    list_per_page = 20
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    fields = ("user", "created_at",)
+    search_fields = ("user",)
+    search_help_text = "Search by user"
+    list_per_page = 20
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    fields = ("performance", "reservation", "row", "seat")
+    search_fields = ("performance",)
+    search_help_text = "Search by performance"
+    list_per_page = 20
