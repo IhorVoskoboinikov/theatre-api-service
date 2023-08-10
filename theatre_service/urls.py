a181fc6b636f368716364a028e3245db69e81ea3
YOUR_NAME <YOUR_EMAIL>
2023-08-10 17:20:28 +0300

    added path to documentation in swagger

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,11 @@ urlpatterns = [
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
 
     path("__debug__/", include("debug_toolbar.urls")),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    )
 ]
