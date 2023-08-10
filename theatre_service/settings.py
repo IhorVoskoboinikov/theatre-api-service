d902270a4083d2d1ee608676c46ae0fb38ab1bfb
YOUR_NAME <YOUR_EMAIL>
2023-08-10 14:41:21 +0300

    added pagination to all pages as general

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -134,6 +134,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
